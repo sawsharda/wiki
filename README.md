8e04b7623b9958ad34c97c0c9e0a579b644e21d3
Sharda Saw <kalsu@Shardas-MacBook-Air.local>
2025-07-26 22:38:32 +0530

    Main

--- README.md
+++ README.md
@@ -1,23 +1,44 @@
 ## How to create from terminal
 
-''' 
+```
 In Terminal
 npm create vite
 In VS
 npm run dev
-'''
+```
 
 ## bootstrap
 
 ### Styling
-'''
+```
 min-vh-100   Minimum height = 100% of the viewport height
-
-'''
-
+```
 
 ### For input
-'''
+```
 form-control  Adds padding, border, shadow, and makes input 100% width
+```
 
-'''
+### For Graph in React
+```
+npm install recharts
+```
+### In React
+```
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid,
+  Tooltip, Legend, ResponsiveContainer
+} from 'recharts';
+
+```
+### In Card & Rows
+```
+{//add padding (eg:pd-4) and margin below(eg: mb-4)}
+<Row className="p-2 mb-4"></Row>
+```
+
+### For down for Phone side by side for laptop 
+```
+<Col xs={12} md={6} lg={4}></Col>
+
+```
--- src/components/Wiki.jsx
+++ src/components/Wiki.jsx
@@ -1,33 +1,106 @@
 import Noimage from "../assets/No-Image-Placeholder.svg.png";
-
+import {Card,
+  Container,
+Row,
+Col,
+Form} from "react-bootstrap";
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid,
+  Tooltip, Legend, ResponsiveContainer
+} from 'recharts';
+const data = [
+  { name: 'Jan', value: 400 },
+  { name: 'Feb', value: 300 },
+  { name: 'Mar', value: 500 },
+  { name: 'Apr', value: 200 },
+];
 export default function Wiki() {
   return (
-    <div>
+    <Container >
       <div className="text-light d-flex align-items-center justify-content-center min-w-0 sm:w-300 md:min-w-400 lg:min-w-500 max-w-80vw">
         <h1 className="">Wikipedia Dashboard</h1>
       </div>
-      <div className="d-flex">
-        <input
-          type="text"
-          name="search"
-          className="form-control"
-          placeholder="Enter a Wikipedia Page Title (eg: Hello)"
-        />
-        <button className="btn btn-primary">Search</button>
-      </div>
-      <div className="d-flex flex-column align-items-center justify-content-center mt-4 border-2 border-secondary rounded p-4">
-        <div>
-          <h1>Title Page#pageid</h1>
-        </div>
-        <div>
-          <div className="h-20 w-20 ">
-          <img src={Noimage} alt="No Image" />
+      <Form>
+        <div className=" d-flex  gap-2 align-items-center justify-content-center">
+            <input
+              type="text"
+              name="search"
+              className="form-control "
+              placeholder="Enter a Wikipedia Page Title (eg: Hello)"
+            />
+         
+             <button className="btn btn-primary ml-2">Search</button>
           </div>
-          <div>
-            <p>No summary provided on the page.Please visit the page for more details.</p>
-          </div>
-        </div>
-      </div>
+      </Form>
+      
+
+       <Card className="p-4">
+        
+      
+        <Card className="p-2 mb-4">
+          <h1 class="text-center color-white">Title Page#pageid</h1>
+        </Card>
+         <Row className="mb-4">
+          <Col xs={12} md={6} lg={4}
+ className="p-3">
+            <img src={Noimage} alt="No Image" style={{width:"20vw"}}/>
+          </Col>
+          <Col xs={12} md={6} lg={4}
+ className="p-3">
+<Card className="p-4" >         
+     <p  >No summary provided on the page.Please visit the page for more details.</p>
+         </Card>
+
+          </Col>
+       </Row>
+       <Row className="mb-4">
+        <Col xs={12} md={6} lg={4}
+className="p-3">
+        <ul class="list-group">
+  <li class="list-group-item active" aria-current="true">Open Original Page</li>
+  <li class="list-group-item">Created on:</li>
+  <li class="list-group-item">Page size:</li>
+  <li class="list-group-item">Last Edited</li>
+  <li class="list-group-item">View in last 30 day:</li>
+</ul>
+        </Col>
+        <Col xs={12} md={6} lg={4}
+ className="p-3">
+     <ul class="list-group">
+  <li class="list-group-item active" aria-current="true">Open Original Page</li>
+  <li class="list-group-item">Created on:</li>
+  <li class="list-group-item">Page size:</li>
+  <li class="list-group-item">Last Edited</li>
+  <li class="list-group-item">View in last 30 day:</li>
+</ul>
+        </Col>
+        <Col xs={12} md={6} lg={4}
+className="p-3">
+        <ul class="list-group">
+  <li class="list-group-item active" aria-current="true">Open Original Page</li>
+  <li class="list-group-item">Created on:</li>
+  <li class="list-group-item">Page size:</li>
+  <li class="list-group-item">Last Edited</li>
+  <li class="list-group-item">View in last 30 day:</li>
+</ul>
+        </Col>
+       </Row>
+       <Card>
+        <div style={{ width: '100%', height: 300 }}>
+      <ResponsiveContainer>
+        <LineChart data={data}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Line type="monotone" dataKey="value" stroke="#8884d8" />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
+       </Card>
+
+
+  
+      </Card>
+    </Container>
   );
 }
